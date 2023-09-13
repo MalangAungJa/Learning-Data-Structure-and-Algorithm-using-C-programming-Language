59935123210ef7529e6032c7b16cbb420318859c
Malang Aung Ja <144532729+MalangAungJa@users.noreply.github.com>
2023-09-13 21:00:00 +0630

    unary_binary_operators_4.c

---
+++ unary_binary_operators_4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    int a = 10;
+    int b = 20;
+    int z = -a; // unary minus
+
+    int x = b - a; // binary minus
+
+    printf("The unary minus = %d\n", z);
+    printf("The binary minus = %d\n", x);
+
+
+    return 0;
+}
